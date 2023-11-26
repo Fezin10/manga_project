0d929c9e46102adc43207b60ed3e6e91f2424ca5
Fezin10 <frdias8183@gmail.com>
2023-11-26 19:01:20 -0300

    templates and static folder and basic files created, index view created but no content

--- manga/mangaweb/views.py
+++ manga/mangaweb/views.py
@@ -1,3 +1,12 @@
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.db.models import Max
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    return render(request, 'mangaweb/index.html')
